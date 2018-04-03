d7b938319985b70176c8a574ce9d6aa15e9640ca
nlpcapstonebot <nelson.liu.2009@gmail.com>
2018-04-02 17:19:36 -0700

    Update aggregator 20180402171936

---
+++ update_aggregator.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+current_date_time="`date +%Y%m%d%H%M%S`";
+
+# go to home
+cd ~
+
+# go to venus directory
+cd venus
+
+# Pull changes from planet
+python planet.py ~/Documents/Github/nlpcapstone.github.io/capstone_2018.ini
+
+# push changes to github
+cd ~/Documents/Github/nlpcapstone.github.io/
+ssh-add ~/.ssh/id_rsa_nlpcapstonebot
+git add -A
+git commit --no-gpg-sign -m "Update aggregator ${current_date_time}"
+git push
